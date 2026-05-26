af55de8602bf30d008f1445ffd4d71a5d229ee37
ankith2409-web <kanchikumarswamy@gmail.com>
2026-05-26 16:01:13 +0530

    Add very basic variables script

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Let's learn about variables and basic math!
+
+# These are variables storing different types of data
+name = "Alice"     # String (text)
+age = 25           # Integer (whole number)
+height = 5.6       # Float (decimal number)
+
+# Printing variables
+print("My name is", name)
+print("I am", age, "years old")
+
+# Basic math
+next_year_age = age + 1
+print("Next year, I will be", next_year_age)
